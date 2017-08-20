92a8fb9f5acc60703a3780e68a4d53b8d52fde7c
Benjamin M6 <benjamynfrank101@gmail.com>
2017-08-20 08:03:11 -0500

    Load the list  of users from the internet (Github API)

--- app/src/main/java/com/m6code/jevloper/MainActivity.java
+++ app/src/main/java/com/m6code/jevloper/MainActivity.java
@@ -1,34 +1,85 @@
 package com.m6code.jevloper;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // create the log tag to log errors to console
+    public static final String LOG_TAG = MainActivity.class.getName();
+
+    /**
+     * URL for user data from Github API
+     * Uses the location (i.e location:lagos) to limit the query of users to a particular location
+     * Uses the language (i.e language:java) to limit the query of users data to a specific language;
+     * users that have java repos in this case
+     */
+    private static final String USER_DATA_REQUEST_URL =
+            "https://api.github.com/search/users?q=location:lagos+language:java";
+
+    /**
+     * Adapter for the list of github users
+     */
+    private UserAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<User> users = new ArrayList<User>();
-        users.add(new User("user1"));
-        users.add(new User("user2"));
-        users.add(new User("user3"));
-        users.add(new User("user4"));
-        users.add(new User("user5"));
-        users.add(new User("user6"));
-
         // Find a reference to the {@link ListView} from the activity_main layout
-        ListView userListView = (ListView)findViewById(R.id.list);
+        ListView userListView = (ListView) findViewById(R.id.list);
 
         // Create a new adapter that takes an empty list of users as input
-        UserAdapter adapter = new UserAdapter(this, users);
+        mAdapter = new UserAdapter(this, new ArrayList<User>());
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        userListView.setAdapter(adapter);
+        userListView.setAdapter(mAdapter);
+
+        UserAsyncTask userTask = new UserAsyncTask();
+        userTask.execute(USER_DATA_REQUEST_URL);
+    }
+
+    /**
+     * {@link AsyncTask} to perform the network request on a background thread and then update
+     * the User Interface with the list of users in the response.
+     */
+    private class UserAsyncTask extends AsyncTask<String, Void, List<User>> {
+        /**
+         * This method runs on a background thread and performs the network request
+         * and returns a list of {@link User}s
+         */
+        @Override
+        protected List<User> doInBackground(String... urls) {
+            // Don't perform the request if no URLs is provided or if the 1st URL is null
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            List<User> result = QueryUtilities.fetchUserData(urls[0]);
+            return result;
+        }
+
+        /**
+         * This method runs on the main UI thread after the background and receives an input return
+         * value from the doInBackground() method
+         */
+        @Override
+        protected void onPostExecute(List<User> data) {
+
+            // Clears the adapter of previous user data
+            mAdapter.clear();
+
+            // If there is a valid list of {@link User}s, then add them to the adapter data
+            // set, which will trigger the ListView to update.
+            if (data !=null && !data.isEmpty()){
+                mAdapter.addAll(data);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/m6code/jevloper/QueryUtilities.java
@@ -0,0 +1,179 @@
+package com.m6code.jevloper;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Benjamin on 20/8/2017.
+ * Helper methods related to requesting and receiving User data from Github
+ */
+
+public final class QueryUtilities {
+    /**
+     * Tag for log messages
+     */
+    public static final String LOG_TAG = QueryUtilities.class.getSimpleName();
+
+    /**
+     * Create a private constructor because no one should ever create a {@link QueryUtilities} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     */
+
+    private QueryUtilities() {
+
+    }
+
+    /**
+     * Query Github dataset and return a {@link User} object to represent a single user
+     */
+    public static List<User> fetchUserData(String requestURL) {
+        // Create URL object
+        URL url = createURL(requestURL);
+
+        // Perform Http request to the URL and receive a JSON response back
+        String JSONResponse = null;
+
+        try {
+            JSONResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error making http request ", e);
+        }
+
+        // Extract relevant fields from the JSON response and create an {@link User} object
+        List<User> users = extractUsers(JSONResponse);
+
+        // Return the {@link User}
+        return users;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createURL(String stringURL) {
+        URL url = null;
+        try {
+            url = new URL(stringURL);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error creating URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL; return a string as the response
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String JSONResponse = "";
+
+        // Check if URL is null, and return early
+        if (url == null) {
+            return JSONResponse;
+        }
+
+        // Make the connection if url in not null
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(15000 /*in milliseconds*/);
+            urlConnection.setConnectTimeout(30000 /*in milliseconds*/);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Check if the request was successful i.e response code 200,
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                JSONResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error reponse code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the user JSON results ", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return JSONResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream,
+                    Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+
+    /**
+     * Return a list of {@link User} objects that has been built
+     * up from parsing a JSON response
+     */
+
+    public static ArrayList<User> extractUsers(String userJSON) {
+        if (TextUtils.isEmpty(userJSON)) {
+            return null;
+        }
+
+        // Create an empty ArrayList that users are added to
+        ArrayList<User> users = new ArrayList<>();
+
+        // Try to parse the SAMPLE_JSON_RESPONSE. If there's a problem with the way the JSON
+        // is formatted, a JSONException exception object will be thrown.
+        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+        try {
+            // build up a list of User objects with the corresponding data.
+            JSONObject baseJsonResponse = new JSONObject(userJSON);
+            JSONArray userArray = baseJsonResponse.getJSONArray("items");
+
+            for (int i = 0; i < userArray.length(); i++) {
+                JSONObject currentUser = userArray.getJSONObject(i);
+
+                // extract the username from the key login
+                String login = currentUser.getString("login");
+
+                User user = new User(login);
+                users.add(user);
+            }
+        }catch (JSONException e){
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+        }
+        return users;
+
+    }
+}
